09e067e336db6de06b71966b0df6d5ce6f5d7343
matt emerson <matthewwemerson@gmail.com>
2020-03-13 13:59:15 +0000

    Added routes, controllers and services for getPubs

---
+++ functions/src/services/pubs.service.js
@@ -0,0 +1,10 @@
+const request = require('request');
+const request_promise = require('request-promise');
+
+const getPubs = async (fields) => {
+    const response = await request_promise('#');
+}
+
+module.exports = {
+    getPubs
+};
