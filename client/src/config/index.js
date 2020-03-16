84a8c89353fa99ebcec7bf2a90f0a96660078824
Richard Waller <Richard.Waller@ibm.com>
2020-03-16 21:21:31 +0000

    ci: can run app in local dev mode

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,10 +1,9 @@
 const port = 4000;
 
 const origins = {
-//   cloud: 'https://reunite.eu-gb.cf.appdomain.cloud',
-  local: `https://us-central1-gauntlet-5ac98.cloudfunctions.net/api/v1`,
+  cloud: `https://us-central1-gauntlet-5ac98.cloudfunctions.net/api/v1`,
   // local: `http://localhost:5000/gauntlet-5ac98/us-central1/api/v1`,
-  // local: `http://localhost:${port}/api/v1`,
+  local: `http://localhost:${port}/v1`,
 //   'local-network': `http://169.254.XXX.XX:${port}`, // Set this to your Private IP address
 };
 
