2d362690d188a055515dd60c15559d007605abdd
CForrest97 <craforrest97@gmail.com>
2020-03-18 15:35:19 +0000

    temp fix for venueId
    Signed-off-by: CForrest97 <craforrest97@gmail.com>

--- functions/src/controllers/drinks.controller.js
+++ functions/src/controllers/drinks.controller.js
@@ -2,7 +2,7 @@ const drinksService = require('../services/wetherspoons.service');
 
 const getDrinks = async (req, res) => {
     try {
-        const venueId = req.body.venueId
+        const venueId = req.body.venueId || 269
         const drinks = await drinksService.getDrinks(venueId);
         res.status(200).send(drinks);
     } catch(err) {
