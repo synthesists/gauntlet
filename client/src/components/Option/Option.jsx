85337f4454755e6b2cd3b0b57904f61fe30b7054
Richard Waller <Richard.Waller@ibm.com>
2020-03-22 15:17:01 +0000

    client feat: minor visual improvements

--- client/src/components/Option/Option.jsx
+++ client/src/components/Option/Option.jsx
@@ -6,7 +6,7 @@ import Card from '../../elements/Card';
 export default ({ name, onClick }) => (
   <div onClick={onClick} className={Option}>
     <Card>
-      <h1>{name}</h1>
+      <h2>{name}</h2>
     </Card>
   </div>
 );
--- client/src/containers/Game/PlayerChoice/PlayerChoice.jsx
+++ client/src/containers/Game/PlayerChoice/PlayerChoice.jsx
@@ -7,6 +7,9 @@ export default ({ player, currentRound, numberOfRounds, options, onChoice }) => 
   console.log('options', options);
   return (
     <div className={view} style={{background: player.colour}}>
+      <h1>{`Round ${currentRound+1} of ${numberOfRounds}`}</h1>
+      <h2>{`${player.name}'s turn`}</h2>
+      <h2>{`Options: `}</h2>
 
       {options.map((option, i) => (
           <Option
