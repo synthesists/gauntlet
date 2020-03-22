aca70d2629e7e49f853fe04b9d56067ec2d94d06
Richard Waller <Richard.Waller@ibm.com>
2020-03-22 15:08:23 +0000

    server test, refactor: fix tests by refactoring getTrees

--- functions/src/services/trees.service.js
+++ functions/src/services/trees.service.js
@@ -1,43 +1,57 @@
-const { getDrinks, getDrink } = require('./wetherspoons.service');
-
 const toString = ({ x, y }) => `${y}-${x}`;
-const getChildren1 = ({ x, y }) => [{ x, y: y + 1 }, { x: x + 1, y: y + 1 }];
+
+const getDivergingChildren = ({ x, y }) => [{ x, y: y + 1 }, { x: x + 1, y: y + 1 }];
+
 const isHalfWay = (r, rounds) => r >= (rounds + 1) / 2;
+
 const getWidth = (r, rounds) => (isHalfWay(r, rounds) ? 2 + rounds - r : 3 + r);
-const getChildren2 = ({ x, y }, rounds) => {
+
+const getConvergingChildren = ({ x, y }, rounds) => {
   const newWidth = getWidth(y + 1, rounds);
-  return [{ x: (x + newWidth - 1) % newWidth, y: y + 1 }, { x: x % newWidth, y: y + 1 }];
+  return [
+    {
+      x: (x + newWidth - 1) % newWidth,
+      y: y + 1,
+    },
+    {
+      x: x % newWidth,
+      y: y + 1,
+    },
+  ];
 };
 
-const tree = async (rounds, venueId) => {
+const getTree = (rounds, getDrink) => {
   let currentRow = [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }];
-  const treesObj = {
+  const tree = {
     start: { children: currentRow.map((child) => toString(child)) },
     end: { children: [] },
   };
-  const allDrinks = await getDrinks(venueId);
   for (let r = 1; r < rounds; r++) {
     let nextRow = [];
 
     currentRow.forEach((node) => {
-      const children = isHalfWay(r, rounds) ? getChildren2(node, rounds) : getChildren1(node);
+      const children = isHalfWay(r, rounds)
+        ? getConvergingChildren(node, rounds)
+        : getDivergingChildren(node);
       nextRow = nextRow.concat(children);
 
-      const drink = getDrink(allDrinks, r, rounds);
-      treesObj[toString(node)] = {
+      tree[toString(node)] = {
         children: children.map((child) => toString(child)),
-        drink,
+        drink: getDrink(),
       };
     });
     currentRow = nextRow;
   }
 
   for (let x = 0; x < 3; x++) {
-    treesObj[toString({ x, y: rounds - 1 })] = {
+    tree[toString({ x, y: rounds - 1 })] = {
       children: ['end'],
-      drink: getDrink(allDrinks),
+      drink: getDrink(),
     };
   }
-  return treesObj;
+  return tree;
 };
-module.exports = { tree };
+
+module.exports = {
+  getTree,
+};
--- functions/src/services/trees.service.test.js
+++ functions/src/services/trees.service.test.js
@@ -1,55 +1,59 @@
-const { tree } = require('./trees.service');
+const { getTree } = require('./trees.service');
+
+const getDrink = () => 'drink';
 
 test('tree of depth 1', () => {
-  expect(tree(1)).toStrictEqual({
+  expect(getTree(1, getDrink)).toStrictEqual({
     start: { children: ['0-0', '0-1', '0-2'] },
-    '0-0': { children: ['end'] },
-    '0-1': { children: ['end'] },
-    '0-2': { children: ['end'] },
+    '0-0': { children: ['end'], drink: 'drink' },
+    '0-1': { children: ['end'], drink: 'drink' },
+    '0-2': { children: ['end'], drink: 'drink' },
     end: { children: [] },
   });
 });
 
 test('tree of depth 3', () => {
-  expect(tree(3)).toStrictEqual({
+  expect(getTree(3, getDrink)).toStrictEqual({
     start: { children: ['0-0', '0-1', '0-2'] },
-    '0-0': { children: ['1-0', '1-1'] },
-    '0-1': { children: ['1-1', '1-2'] },
-    '0-2': { children: ['1-2', '1-3'] },
-    '1-0': { children: ['2-2', '2-0'] },
-    '1-1': { children: ['2-0', '2-1'] },
-    '1-2': { children: ['2-1', '2-2'] },
-    '1-3': { children: ['2-2', '2-0'] },
-    '2-0': { children: ['end'] },
-    '2-1': { children: ['end'] },
-    '2-2': { children: ['end'] },
+    '0-0': { children: ['1-0', '1-1'], drink: 'drink' },
+    '0-1': { children: ['1-1', '1-2'], drink: 'drink' },
+    '0-2': { children: ['1-2', '1-3'], drink: 'drink' },
+    '1-0': { children: ['2-2', '2-0'], drink: 'drink' },
+    '1-1': { children: ['2-0', '2-1'], drink: 'drink' },
+    '1-2': { children: ['2-1', '2-2'], drink: 'drink' },
+    '1-3': { children: ['2-2', '2-0'], drink: 'drink' },
+    '2-0': { children: ['end'], drink: 'drink' },
+    '2-1': { children: ['end'], drink: 'drink' },
+    '2-2': { children: ['end'], drink: 'drink' },
     end: { children: [] },
   });
 });
 
 test('tree of depth 5', () => {
-  expect(tree(5)).toStrictEqual({
+  expect(getTree(5, getDrink)).toStrictEqual({
     start: { children: ['0-0', '0-1', '0-2'] },
-    '0-0': { children: ['1-0', '1-1'] },
-    '0-1': { children: ['1-1', '1-2'] },
-    '0-2': { children: ['1-2', '1-3'] },
-    '1-0': { children: ['2-0', '2-1'] },
-    '1-1': { children: ['2-1', '2-2'] },
-    '1-2': { children: ['2-2', '2-3'] },
-    '1-3': { children: ['2-3', '2-4'] },
-    '2-0': { children: ['3-3', '3-0'] },
-    '2-1': { children: ['3-0', '3-1'] },
-    '2-2': { children: ['3-1', '3-2'] },
-    '2-3': { children: ['3-2', '3-3'] },
-    '2-4': { children: ['3-3', '3-0'] },
-    '3-0': { children: ['4-2', '4-0'] },
-    '3-1': { children: ['4-0', '4-1'] },
-    '3-2': { children: ['4-1', '4-2'] },
-    '3-3': { children: ['4-2', '4-0'] },
-    '4-0': { children: ['end'] },
-    '4-1': { children: ['end'] },
-    '4-2': { children: ['end'] },
+    '0-0': { children: ['1-0', '1-1'], drink: 'drink' },
+    '0-1': { children: ['1-1', '1-2'], drink: 'drink' },
+    '0-2': { children: ['1-2', '1-3'], drink: 'drink' },
+    '1-0': { children: ['2-0', '2-1'], drink: 'drink' },
+    '1-1': { children: ['2-1', '2-2'], drink: 'drink' },
+    '1-2': { children: ['2-2', '2-3'], drink: 'drink' },
+    '1-3': { children: ['2-3', '2-4'], drink: 'drink' },
+    '2-0': { children: ['3-3', '3-0'], drink: 'drink' },
+    '2-1': { children: ['3-0', '3-1'], drink: 'drink' },
+    '2-2': { children: ['3-1', '3-2'], drink: 'drink' },
+    '2-3': { children: ['3-2', '3-3'], drink: 'drink' },
+    '2-4': { children: ['3-3', '3-0'], drink: 'drink' },
+    '3-0': { children: ['4-2', '4-0'], drink: 'drink' },
+    '3-1': { children: ['4-0', '4-1'], drink: 'drink' },
+    '3-2': { children: ['4-1', '4-2'], drink: 'drink' },
+    '3-3': { children: ['4-2', '4-0'], drink: 'drink' },
+    '4-0': { children: ['end'], drink: 'drink' },
+    '4-1': { children: ['end'], drink: 'drink' },
+    '4-2': { children: ['end'], drink: 'drink' },
     end: { children: [] },
+  });
+});
 
   });
 });
