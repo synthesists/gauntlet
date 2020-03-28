9a4edae2994d209c1c4fe18a92b93c758389f1a2
Richard Waller <Richard.Waller@ibm.com>
2020-03-28 13:04:32 +0000

    client feat: remove header

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 // import Setup from './containers/Setup/Setup';
-import Header from './components/Header';
 import Game from './containers/Game/Game';
 
 export default () => {
   return (
     <div>
-      <Header />
       <main>
         <Game venue={{ id: 670 }} />
       </main>
